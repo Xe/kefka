a7d7c1016ca4feab1125d1a8a08db7cc8df6a014
Xe Iaso <me@xeiaso.net>
2026-05-01 13:31:30 -0400

    feat(command): add quickjs as qjs
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- command/registry/wasmprog/wasmprog.go
+++ command/registry/wasmprog/wasmprog.go
@@ -2,6 +2,7 @@ package wasmprog
 
 import (
 	"tangled.org/xeiaso.net/kefka/command/internal/python3"
+	"tangled.org/xeiaso.net/kefka/command/internal/qjs"
 	"tangled.org/xeiaso.net/kefka/command/registry"
 )
 
@@ -9,4 +10,6 @@ func Register(reg *registry.Impl) {
 	// multiple entries for Python should exist
 	reg.Register("python", python3.Impl{})
 	reg.Register("python3", python3.Impl{})
+
+	reg.Register("qjs", qjs.Impl{})
 }
